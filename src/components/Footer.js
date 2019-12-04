0d7b77dc6aa03715ee8ca7a9bd6e2284cc36c6ac
Raul Dronca <rauldronk@gmail.com>
2019-12-04 16:21:53 +0200

    small footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,23 +7,15 @@ const FooterWrapper = styled.footer`
   justify-content: flex-end;
   align-items: center;
   flex-direction: column;
+`
 
-  span {
-    color: rgba(255, 255, 255, 0.6);
-
-    a {
-      color: #f5576c;
-      text-decoration: none;
-    }
-  }
+const Copyright = styled.span`
+  color: rgba(255, 255, 255, 0.6);
 `
 
 const Footer = () => (
   <FooterWrapper>
-    <span>All screenshots © of their respective owners.</span>
-    <span>
-      Built by <a href="https://twitter.com/raul_dronca">@raul_dronca</a>
-    </span>
+    <Copyright>All screenshots © of their respective owners.</Copyright>
   </FooterWrapper>
 )
 
